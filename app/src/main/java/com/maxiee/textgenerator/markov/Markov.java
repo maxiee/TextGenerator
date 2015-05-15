1c9233bbb253b9081acbfb61b502e2e7ecc28348
Maxiee <maxieewong@gmail.com>
2015-05-15 12:22:43 +0800

    generator and GeneratorFragment.

--- app/src/main/java/com/maxiee/textgenerator/markov/Markov.java
+++ app/src/main/java/com/maxiee/textgenerator/markov/Markov.java
@@ -1,8 +1,13 @@
 package com.maxiee.textgenerator.markov;
 
+import android.util.Log;
+
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,14 +19,51 @@ public class Markov {
     private static String mBlackList1 = "[^\u4e00-\u9fa5]";
     private static String mBlackList2 = "[一]";
 
-    public static String generateText() {
-        return "";
+    public static String generateText(JSONObject md) throws JSONException {
+        JSONObject model = md;
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(model.length());
+        String currentFragment = model.names().getString(randomIndex);
+        String ret = "";
+        for (int i = 0; i < 30; i++) {
+            String newCahr = getNextChar(model, currentFragment);
+            if (newCahr == null) {
+                return "";
+            }
+            ret += newCahr;
+            currentFragment = newCahr;
+        }
+        return ret;
+    }
+
+    private static String getNextChar(JSONObject model, String fragment) throws JSONException {
+        ArrayList<String> chars = new ArrayList<>();
+        if (!model.has(fragment)) {
+            Log.d("maxiee", "模型无此fragment");
+            return null;
+        }
+        JSONObject freqModel = model.getJSONObject(fragment);
+        Iterator<String> keys = freqModel.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            int freq = 0;
+            try {
+                freq = freqModel.getInt(key);
+                Log.d("maxiee", "连接词:"+key+"频数"+String.valueOf(freq));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            for (int i=0; i<freq; i++) {
+                chars.add(key);
+            }
+        }
+        Random rand = new Random();
+        return chars.get(rand.nextInt(chars.size()));
     }
 
     public static JSONObject generateModel(ArrayList<String> textList, int order) {
         JSONObject model = new JSONObject();
         for (String text:textList) {
-//            text = pureText(text);
             for (int i = 0; i < text.length() - order; i++) {
                 String fragment = text.substring(i, i+order);
                 JSONObject fragmentModel;
@@ -44,6 +86,8 @@ public class Markov {
                 } else {
                     try {
                         int count = fragmentModel.getInt(String.valueOf(nextChar));
+                        count += 1;
+                        fragmentModel.put(String.valueOf(nextChar), count);
                     } catch (Exception e) {continue;}
                 }
             }
--- app/src/main/java/com/maxiee/textgenerator/ui/GeneratorFragment.java
+++ app/src/main/java/com/maxiee/textgenerator/ui/GeneratorFragment.java
@@ -1,33 +1,87 @@
 package com.maxiee.textgenerator.ui;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.InputType;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.maxiee.textgenerator.R;
+import com.maxiee.textgenerator.database.DatabaseHelper;
+import com.maxiee.textgenerator.database.ModelTable;
+import com.maxiee.textgenerator.markov.Markov;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by maxiee on 15-5-14.
  */
 public class GeneratorFragment extends Fragment{
 
-    private EditText mGenerateredText;
+    private TextView mGenerateredText;
     private Button mGenerateButton;
+    private DatabaseHelper mDatabaseHelper;
+    private JSONObject model;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_generator, container, false);
+        mDatabaseHelper = DatabaseHelper.instance(getActivity());
 
-        mGenerateredText = (EditText) rootView.findViewById(R.id.text);
-        mGenerateredText.setInputType(InputType.TYPE_NULL);
+        mGenerateredText = (TextView) rootView.findViewById(R.id.text);
         mGenerateButton = (Button) rootView.findViewById(R.id.generate);
+        mGenerateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGenerateredText.setText(generateText());
+            }
+        });
 
         return rootView;
+    }
+
+    public String generateText() {
+        String ret = "";
+        Cursor cursor = mDatabaseHelper.getReadableDatabase().query(
+            ModelTable.NAME,
+                    new String[] {
+                            ModelTable.CONTENT
+                    },
+                    ModelTable.ID + "=?",
+                    new String[] {"1"},
+                    null, null, null
+        );
+
+        if (cursor.getCount() >= 1) {
+            cursor.moveToFirst();
+            Log.d("maxiee", "加载模型");
+            try {
+                model = new JSONObject(cursor.getString(
+                        cursor.getColumnIndex(ModelTable.CONTENT)
+                ));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (model != null) {
+            Log.d("maxiee", "模型不为空");
+            try {
+                ret = Markov.generateText(model);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.d("maxiee", ret);
+        return ret;
     }
 }
